a74ff09075b536efc18c19bd41a1342836bba9af
SYadda <90630675+SYadda@users.noreply.github.com>
2023-05-26 14:53:01 +0800

    new backend server and database
    only implement "/api/expenses/{expenseID}"

---
+++ backend/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify, json
+import pandas as pd
+
+app = Flask(__name__)
+expense = pd.read_csv('expense.csv')
+
+
+@app.route('/api/expenses/<int:expenseID>', methods=['GET', 'POST'])
+def get_expense_by_id(expenseID):
+    if request.method == 'GET':
+        return jsonify(expense.loc[expense['expenseID'] == expenseID].to_json())
+    # elif request.method == 'POST':
+    #     # 处理 POST 请求
+    #     json_data = request.get_json()
+    #     if json_data:
+    #         data = {'message': 'Received data: {}'.format(json_data)}
+    #         return jsonify(data)
+    #     else:
+    #         return jsonify({'error': 'Invalid JSON'}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
