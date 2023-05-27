8e620eaa7744fab0a26383e1b623dc22d64afd72
hehan2 <hehan2002@126.com>
2023-05-27 15:48:06 +0800

    fix-bug

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify, json
+from flask_cors import CORS, cross_origin
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 expense = pd.read_csv('expense.csv')
 
 
 @app.route('/api/expenses/<int:expenseID>', methods=['GET', 'POST'])
+@cross_origin()
 def get_expense_by_id(expenseID):
     if request.method == 'GET':
         return jsonify(expense.loc[expense['expenseID'] == expenseID].to_json())
