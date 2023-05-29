eca49d8cfea65bf3b7a876264e7d693d803adbd7
wangci76 <82521714+wangci76@users.noreply.github.com>
2023-05-30 03:06:20 +0800

    response-test

--- src/views/expenses/admin/components/RaddarChart.vue
+++ src/views/expenses/admin/components/RaddarChart.vue
@@ -124,9 +124,10 @@
         .then(response => {
           console.log(response.data)
         })
-        .catch(function (error) { // 请求失败处理
-            console.log(error);
-          });
+        .catch(error => {
+          const errorMessage = error.response.data.error;
+          alert(errorMessage);
+        });
       },
       add() {
         var id=this.ruleForm.expenseID;
